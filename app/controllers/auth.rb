eb8892443bdfff920a37f225f48fdfb70f733c53
Matt Evanoff <mevanoff24@gmail.com>
2015-02-07 09:15:30 -0800

    hope this works

--- app/controllers/auth.rb
+++ app/controllers/auth.rb
@@ -1,34 +1,39 @@
 
-
 get '/login' do
 
+
 end
 
-post '/login' do
-  @user = User.find_by(username: params[:username])
-  if @user && @user.password == params[:password]
-    set_current_user(@user)
+post '/login' do   
+  user = User.find_by(username: params[:username]).try(:authenticate,
+  params[:password])   # @user = User.find_by(username: params[:username])   #
+  if user
+    session[:user_id] = user.id 
+    redirct '/workout'   
+  else
+    redirect '/?error=Username or Password Incorrect'
+
   end
 end
-
-
-
-
 
 get '/signup' do
   # erb :signup
 end
 
 post '/signup' do
-  user = User.new(name: params[:name], username: params[:username], email: params[:email], password: params[:passowrd])
+  user = User.new(params[:user])
   if user.save
+    set_current_user
     redirect '/workout'
   else
-    "You have a problem"
+        redirect '/signup?error=Passowrds do not match'
+
+    #add error message
     # erb :signup
   end
 end
 
 get '/signout' do
-  logout
+  session[:user_id] = nil
+  redirect '/'
 end
--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,16 +1,15 @@
 helpers do
 
-  def current_user
-    @current_user ||= User.find(session[:id]) if session[:id]
-  end
+  def current_user @current_user ||= User.find(session[:user_id]) if
+  session[:user_id] end
 
   def logout
     session.clear
-    @current_user = nil
+    current_user = nil
   end
 
-  def set_current_user(user)
-    session[:id] = user.id
+  def set_current_user
+    session[:user_id] = user.id
   end
 
 end
