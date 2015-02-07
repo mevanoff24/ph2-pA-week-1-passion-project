232b862b9b8d46a203ee427912d9b735839887ce
Matt Evanoff <mevanoff24@gmail.com>
2015-02-07 10:46:32 -0800

    start parse

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,18 +4,25 @@
 # Workout.create(name: "Get Big")
 
 
-def write_json
-  events_json = []
-  collection.all.each do |event|
-    event_json = {
-      "name" => event.name.text,
-      "muscle_targeted" => event.muscle_targeted.text,
-      "rating" => event.rating,
-    }
-    events_json << event_json
-  end
-  File.open("public/event.json","w") do |f|
-    f.write(events_json.to_json)
+# def write_json
+#   events_json = []
+#   collection.all.each do |event|
+#     event_json = {
+#       "name" => event.name.text,
+#       "muscle_targeted" => event.muscle_targeted.text,
+#       "rating" => event.rating,
+#     }
+#     events_json << event_json
+#   end
+#   File.open("public/event.json","w") do |f|
+#     f.write(events_json.to_json)
+#   end
+# end
+
+
+exercises = JSON.load(open("exercise.json").read)
+
+exercises["collection"].each do |key, value|
+    Exercise.create(name: value["name"])
   end
 end
-
